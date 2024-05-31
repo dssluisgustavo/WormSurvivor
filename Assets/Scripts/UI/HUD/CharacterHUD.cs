ba485f780b9c0e652da83d67c9cd0bedafd0a6ab
Tallon <srmarcovich@gmail.com>
2024-05-31 20:33:00 -0300

    Fixing characterHUD namespace

--- Assets/Scripts/UI/HUD/CharacterHUD.cs
+++ Assets/Scripts/UI/HUD/CharacterHUD.cs
@@ -2,19 +2,22 @@ using TMPro;
 using UnityEngine;
 using Worm;
 
-public class CharacterHUD : MonoBehaviour
+namespace UI.HUD
 {
-    public TextMeshProUGUI characterName;
-    public HealthHUD healthHUD;
-    public StaminaHUD staminaHUD;
-
-    public WormController WormControllerReference { get; private set; }
-
-    public void Initialize(WormController wormController)
+    public class CharacterHUD : MonoBehaviour
     {
-        WormControllerReference = wormController;
-        
-        healthHUD.Initialize(wormController.health);
-        staminaHUD.Initialize(wormController.stamina);
+        public TextMeshProUGUI characterName;
+        public HealthHUD healthHUD;
+        public StaminaHUD staminaHUD;
+
+        public WormController WormControllerReference { get; private set; }
+
+        public void Initialize(WormController wormController)
+        {
+            WormControllerReference = wormController;
+
+            healthHUD.Initialize(wormController.health);
+            staminaHUD.Initialize(wormController.stamina);
+        }
     }
 }
