107656bb2fd4782b5a1f464d4f0a6ba15fe74993
Tallon <srmarcovich@gmail.com>
2024-05-31 20:34:42 -0300

    Fixing the wormspawner and gamemanager namespaces

--- Assets/Scripts/Managers/WormSpawner.cs
+++ Assets/Scripts/Managers/WormSpawner.cs
@@ -1,35 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
+using UI.HUD;
 using UnityEngine;
 using Worm;
 
-public class WormSpawner : MonoBehaviour
+namespace Managers
 {
-    public Transform[] spawnSpots;
-    public RectTransform characterHUDParent;
-    
-    public GameObject characterPrefab;
-    public CharacterHUD characterHUDPrefab;
-
-    private List<WormController> worms = new();
-    public WormController[] Worms => worms.ToArray();
-    
-    public WormController SpawnWorm()
+    public class WormSpawner : MonoBehaviour
     {
-        if (spawnSpots.Length == worms.Count)
-        {
-            Debug.LogError("No more spawn spots available");
-            return null;
-        }
-        
-        var spot = spawnSpots[worms.Count];
-        var character = Instantiate(characterPrefab, spot.position, Quaternion.identity);
-        var characterHUD = Instantiate(characterHUDPrefab, characterHUDParent);
+        public Transform[] spawnSpots;
+        public RectTransform characterHUDParent;
+    
+        public GameObject characterPrefab;
+        public CharacterHUD characterHUDPrefab;
 
-        var worm = character.GetComponentInChildren<WormController>();
-        characterHUD.Initialize(worm);
+        private List<WormController> worms = new();
+        public WormController[] Worms => worms.ToArray();
+    
+        public WormController SpawnWorm()
+        {
+            if (spawnSpots.Length == worms.Count)
+            {
+                Debug.LogError("No more spawn spots available");
+                return null;
+            }
         
-        worms.Add(worm);
-        return worm;
+            var spot = spawnSpots[worms.Count];
+            var character = Instantiate(characterPrefab, spot.position, Quaternion.identity);
+            var characterHUD = Instantiate(characterHUDPrefab, characterHUDParent);
+
+            var worm = character.GetComponentInChildren<WormController>();
+            characterHUD.Initialize(worm);
+        
+            worms.Add(worm);
+            return worm;
+        }
     }
 }
